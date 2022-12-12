728ba97a0ca8a1e3c5bdbc2197f1eeae2b85380a
georgi vachev <georgi.vachev@abv.bg>
2022-12-12 15:38:42 +0200

    development overview

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+const HEIGHT = canvas.height;
+const WIDTH = canvas.width;
+
+
+
+// define objects
+    // ball = { x, y, size, image, onCollision(), }
+    // paddle = { x, y, size, image, onCollision(), slide() }
+    // brick = { x, y, size, image, onCollision() }
+
+// create animation loop with render() function using requestAnimationFrame
+
+// render bricks, ball, and paddle
+
+// implement ball / paddle physics
+
+// implement ball / bricks physics
+
+// implement mouse tracking and paddle movement
+
+
+
+// create menu with 'settings' and 'start game' options
